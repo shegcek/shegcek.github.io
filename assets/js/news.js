7b98c71c7a50923589b186c3ef1e2200848c526c
Aswathy Saji <69248795+aswathysaji@users.noreply.github.com>
2020-10-28 12:21:06 +0530

    Update news.js

--- assets/js/news.js
+++ assets/js/news.js
@@ -80,56 +80,11 @@
 
     return false;
   });
-
-  // Porfolio isotope and filter
-  $(window).on('load', function() {
-    var portfolioIsotope = $('.portfolio-container').isotope({
-      itemSelector: '.portfolio-item',
-      layoutMode: 'fitRows'
-    });
-
-    $('#portfolio-flters li').on('click', function() {
-      $("#portfolio-flters li").removeClass('filter-active');
-      $(this).addClass('filter-active');
-
-      portfolioIsotope.isotope({
-        filter: $(this).data('filter')
-      });
-      aos_init();
-    });
-
     // Initiate venobox (lightbox feature used in portofilo)
     $(document).ready(function() {
       $('.venobox').venobox();
     });
   });
-
-   // Testimonials carousel (uses the Owl Carousel library)
-   $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      768: {
-        items: 2
-      },
-      900: {
-        items: 3
-      }
-    }
-  });
-
-  // Portfolio details carousel
-  $(".portfolio-details-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
-
   // Init AOS
   function aos_init() {
     AOS.init({
@@ -140,5 +95,4 @@
   $(window).on('load', function() {
     aos_init();
   });
-
-})(jQuery);
+(jQuery);
